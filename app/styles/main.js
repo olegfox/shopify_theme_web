e614805e5d4d4e1572798f7fedf0d95645baf6fa
olegfox <info@olegfox.com>
2015-11-20 18:17:19 +0300

    first commit

---
+++ app/styles/main.js
@@ -0,0 +1,53 @@
+$(function(){
+  /**
+   * Cлайдер с товарами на главной
+   */
+  $('.carousel ul').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 5
+  });
+
+  /**
+   * Каталог товаров
+   */
+  $('.items-catalog').imagesLoaded( function() {
+    $('.items-catalog ul').masonry({
+      // options
+      itemSelector : 'li',
+      'containerStyle' : {
+        position: 'relative'
+      },
+      columnWidth: 20
+    });
+  });
+
+  /**
+   * Обработчик нажатия на стрелку на главной странице
+   */
+  $('a[href="#main-catalog"]').click(function(e){
+    console.log('click');
+    e.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, {
+      duration: 1000
+    });
+  });
+
+  /**
+   * Обработчик скролла
+   */
+  $(window).scroll(function(){
+    $('header .wrap').css({
+      'opacity': 1 - $(this).scrollTop() / ($('body, html').height() - $('.wrap-header-bar').height() * 2)
+    });
+
+    if($(this).scrollTop() >= 200){
+      $('.header-bar-white').switchClass('header-bar-white', 'header-bar-dark', 500);
+    } else {
+      $('.header-bar-dark').switchClass('header-bar-dark', 'header-bar-white', 500);
+    }
+  });
+});
